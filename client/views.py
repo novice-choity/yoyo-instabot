d9c74b60a87488f38612ed46b6dc4586d0de5f5e
Mahmudul Hasan <ikhtiar@Mahmuduls-MacBook-Air.local>
2019-05-12 17:23:13 +0600

    requirements file modified and instagram api and bot added

--- client/views.py
+++ client/views.py
@@ -1,7 +1,20 @@
 from django.views.generic import TemplateView
 
+from api_bot import API
 from permission.mixins import ClientRequiredMixin
 
 
 class ClientHome(ClientRequiredMixin, TemplateView):
     template_name = 'client/home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        client_api = API()
+        login = client_api.login(username='xalien.007', password='Xalien007')
+        print(client_api.username)
+        print(client_api.rank_token)
+        print(client_api.search_location(lat=23.8103, lng=90.4125, query='food'))
+        print(client_api.last_json)
+        client_api.search_username('xalien')
+        print(client_api.last_json)
+        return context
